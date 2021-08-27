c5cfd322c0574be13457c633ff8c75076c42b388
MichaelBHerman <michaelbherman@gmail.com>
2021-08-27 14:30:41 -0600

    User input added by prompts

--- DayTripGenerator.js
+++ DayTripGenerator.js
@@ -19,3 +19,30 @@ let myDestination = getRandomItem(destinations);
 
 console.log(destinations[myDestination]); //this will log the item at the index of [number] 
 
+let myRestaurant = getRandomItem(restaurants);
+
+console.log(restaurants[myRestaurant]); //logs a random restaurant
+
+let myTransport = getRandomItem(modeOfTransport);
+
+console.log(modeOfTransport[myTransport]); //logs a random mode of transportation
+
+let myEntertainment = getRandomItem(entertainment);
+
+console.log(entertainment[myEntertainment]); //logs a random form of entertainment
+
+window.alert("Your destination is" + " " + (destinations[myDestination]) +" " + "Your restaurant is a" + " " + 
+(restaurants[myRestaurant])+" " + "Your mode of transportation is" + " " + (modeOfTransport[myTransport])+" " + 
+"Your entertainment will be" + " " + (entertainment[myEntertainment]));
+//clean the presentation up....line breaks??
+
+let userInput = window.prompt("Are you happy with the results?")
+
+//window.prompt "are you happy with the results?"
+while(userInput === "no"){
+    let myDestination = getRandomItem(destinations); window.alert("your destination is" + (destinations[myDestination])) 
+    let userInput1 = window.prompt("Are you happy with the result?");
+    if (userInput1 === "yes"){
+        break; //user input yes is not breaking the loop....find solution!!!!!
+    }
+}
